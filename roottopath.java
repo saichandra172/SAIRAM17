ad99dd366c4bc99049020e5e05dac8895fd965b0
saichandra172 <saichandra1725@gmail.com>
2018-07-13 14:04:29 +0530

    first commit

---
+++ roottopath.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Path2{
+    
+    
+    public ArrayList<ArrayList<Integer>> pathSum(TreeNode root, int sum) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> temp = new ArrayList<Integer>();
+        if(root == null)
+            return null;
+        // temp.add(root.val);
+        pathSum(root, sum, result, temp);
+        return result;
+        
+    }
+    public void pathSum(TreeNode node, int sum, ArrayList<ArrayList<Integer>> result, ArrayList<Integer> temp) {
+        if(node == null)
+            return;
+        int currentVal = node.val;
+        temp.add(currentVal);
+        if(node.left == null && node.right == null){
+            if(sum - currentVal == 0){
+                result.add(new ArrayList<Integer>(temp));
+            }
+        }
+        pathSum(node.left, sum - node.val, result, temp);
+        pathSum(node.right, sum - node.val, result, temp);
+        temp.remove(temp.size() - 1);
+        
+    }
+    
+    
+    
+    
+    
+    public static void main(String[] args){
+    }
+}
