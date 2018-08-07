b2192f0483bded018beb3584887983594cf427c4
saichandra172 <41150071+saichandra172@users.noreply.github.com>
2018-08-07 14:53:40 +0530

    Add files via upload

---
+++ sairam.py
@@ -0,0 +1,15 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import random
+>>> print("hello sai")
+hello sai
+>>> i=1
+>>> for i in range(1,10):
+	num=random.randint(1,10)
+	print("enter ur guess")
+	guess=urguess()
+	if(guess==num):
+		print("correct")
+	else:
+		print("no")
+[
